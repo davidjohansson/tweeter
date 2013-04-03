48606c950f47998444c236c37ee4f7a61b15f054
davidjohansson <david.johansson@atex.com>
2013-04-03 19:54:06 +0200

    keys template

---
+++ twitterkeys_template.js
@@ -0,0 +1,4 @@
+exports.token = '3XXXXXXXXX89-3CbAPSxXXXXXXXXXXy42A9ddvQkFs96XXXXXXX';
+exports.secret = 'HHXXXXXesTKZ4bLllXXXXXXXXXX8zAaU';
+exports.consumerKey = "XXXXXXXbgfJRXXXXXXXX";
+exports.consumerSecret = "9XXXXXXXXXQJ9U8VuoNMXXXXXXXXX";
